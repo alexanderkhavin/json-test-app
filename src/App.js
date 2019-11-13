4dae25e0dc2524e4ba80f7c446aadd6177644001
Alexander Khavin <khavin2614@gmail.com>
2019-11-13 15:05:45 +0700

    Done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Data from './data.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+          data: []
+        }
+  }
+
+  componentDidMount() {
+    return fetch(`https://bitbucket.org/GuestOne/goodline-test-task/raw/aa16bc87df8355deef678f4b69a2b6d9fe0f9832/frontend/data.json`)
+      .then( (response) => response.json() )
+      .then ((responseJson) => {
+        this.setState({
+          data: responseJson,
+        });
+        console.log(responseJson);
+    })
+    .catch((error) => {
+      console.log(error);
+      
+    });
+  }
+  render() {
+    let info = this.state.data.map((item, key) => {
+      return(
+        <tr key={key} style={{fontSize: '1.5vw'}}><td>{item.name}</td><td>{item.id}</td><td>{item.phone}</td><td>{item.age}</td></tr>
+      )
+    })
+    return (
+      <table>
+        <thead style={{backgroundColor: '#ccc'}}>
+          <tr style={{fontSize: '1.5vw'}}>
+            <th style={{width: '20vw'}}>Name</th>
+            <th style={{width: '40vw'}}>ID</th>
+            <th style={{width: '30vw'}}>Phone</th>
+            <th style={{width: '10vw'}}>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {info}
+        </tbody>
+        </table>
+    );
+  }
 }
 
 export default App;
